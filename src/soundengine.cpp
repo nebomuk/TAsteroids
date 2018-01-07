5ae418c10377721442bef651feb0f7ab5aab8650
taiko000 <taiko000@gmail.com>
2018-01-07 20:05:57 +0100

    fixed opengl related crash on windows, fixed temp file error

--- src/soundengine.cpp
+++ src/soundengine.cpp
@@ -7,32 +7,35 @@
 SoundEngine::SoundEngine(QObject * parent) : QObject(parent)
 {
 	loopDuplicateCount_ = 4;
+    copyFilesToTempDir_ = false;
 }
 
-void SoundEngine::loadSounds(const QStringList& fileNames)
+void SoundEngine::loadSounds(const QStringList& filePaths)
 {
-    for(QString sourceFileName : fileNames)
+    for(QString sourceFilePath : filePaths)
 	{
-			QString fileName = QFileInfo(sourceFileName).fileName(); // returns file name without path
+        QString fileName = QFileInfo(sourceFilePath).fileName();
 
-			// copy the files to the temp directory
-			QFile::copy(sourceFileName,QDir::tempPath() + "/tmp_" + fileName);
-			QString tempFileName(QDir::tempPath() + "/tmp_" + fileName);
-
-			// save the absolute file names in order to delete them in the dtor
-			tempFileNames_ << tempFileName;
+            if(copyFilesToTempDir_)
+            {
+                QFile::copy(sourceFilePath,QDir::tempPath() + "/tmp_" + fileName);
+                sourceFilePath = QDir::tempPath() + "/tmp_" + fileName;
+                // save the absolute file names in order to delete them in the dtor
+                tempFileNames_ << sourceFilePath;
+            }
 
 			if(fileName.startsWith("loop", Qt::CaseInsensitive))
 			{
 					for(int i = 0; i< loopDuplicateCount_; ++i)
 					{
-                                                        ISound * snd = new Sound();
-							snd->setFile(qPrintable(QDir::toNativeSeparators(tempFileName))); // required for SDL, on windows C:/path becomes C:\path
+                            ISound * snd = new Sound();
+                            snd->setFile(qPrintable(QDir::toNativeSeparators(sourceFilePath))); // required for SDL, on windows C:/path becomes C:\path
 							soundPlayers_.insert(fileName+ '_' + QString::number(i),snd); // loop_sound.wav_0, loop_sound.wav_1 etc
 					}
 			}
-                        ISound * snd = new Sound();
-			snd->setFile(qPrintable(QDir::toNativeSeparators(tempFileName)));
+
+            ISound * snd = new Sound();
+            snd->setFile(qPrintable(QDir::toNativeSeparators(sourceFilePath)));
 			soundPlayers_.insert(fileName,snd);
 	}
 
@@ -75,23 +78,28 @@ void SoundEngine::stopAll()
     }
 }
 
+void SoundEngine::removeTemporaryFiles()
+{
+    bool tempRemoveFailed = false;
+    for(QString file : tempFileNames_)
+    {
+        QDir dir;
+        if(!dir.remove(file))
+            tempRemoveFailed = true;
+    }
+    if(tempRemoveFailed)
+    {
+        QMessageBox::warning(NULL,tr("Could not delete temporary files"),
+                             tr("One ore more temporary sound files could not be removed."
+                                "You can clean up the temp_* files yourself in ") + QDir::tempPath());
+    }
+}
+
 SoundEngine::~SoundEngine()
 {
 	// if the application crashes here, check for multiple deletes (and multiple inserts of the same new'ed value)
 		qDeleteAll(soundPlayers_);
 
 		// destroy the temporary files that have been created before
-		bool tempRemoveFailed = false;
-        for(QString file : tempFileNames_)
-		{
-			QDir dir;
-			if(!dir.remove(file))
-				tempRemoveFailed = true;
-		}
-		if(tempRemoveFailed)
-		{
-			QMessageBox::warning(NULL,tr("Could not delete temporary files"),
-								 tr("One ore more temporary sound files could not be removed."
-									"You can clean up the temp_* files yourself in ") + QDir::tempPath());
-		}
+        removeTemporaryFiles();
 }
